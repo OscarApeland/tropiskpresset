152f28de7d822c0bab4fa0c32a4e5a855b0e60b9
Oscar Apeland <oscar.apeland@icloud.com>
2018-08-06 20:48:39 +0200

    Initial Commit

---
+++ PianoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PianoApp
+//
+//  Created by Oscar Apeland on 06/08/2018.
+//  Copyright © 2018 OleR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
